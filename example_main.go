e50e41c5e2d0b0c18b20059ab6d28329bfd10767
idea_wj <idea_wj@163.com>
2019-06-06 00:20:59 +0800

    加入爬虫规则示例

--- example_main.go
+++ example_main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/jason-wj/pholcus/exec"
-	//_ "github.com/jason-wj/pholcus_lib" // 此为公开维护的spider规则库
+	_ "github.com/jason-wj/pholcus/pholcus_lib" // 此为公开维护的spider规则库
 	// _ "github.com/jason-wj/pholcus_lib_pte" // 同样你也可以自由添加自己的规则库
 )
 
