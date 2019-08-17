5ff4d32f10942852c8a169dcd9b3f93fff43f66f
idea_wj <idea_wj@163.com>
2019-08-17 10:53:56 +0800

    有失败记录时，可手动设置是否需要全站爬取

--- app/spider/spider.go
+++ app/spider/spider.go
@@ -32,7 +32,7 @@ type (
 		Namespace                 func(self *Spider) string                                  // 命名空间，用于输出文件、路径的命名
 		SubNamespace              func(self *Spider, dataCell map[string]interface{}) string // 次级命名，用于输出文件、路径的命名，可依赖具体数据内容
 		RuleTree                  *RuleTree                                                  // 定义具体的采集规则树
-		ContinueSpiderWithFailure bool                                                       // 如果启动时，监测到历史记录中有爬取失败的记录，是否继续爬取该任务
+		ContinueSpiderWithFailure bool                                                       // 如果启动监测到历史记录中有爬取失败的记录时，true:任务和历史错误同时爬取，false：只爬取历史错误记录
 
 		// 以下字段系统自动赋值
 		id        int               // 自动分配的SpiderQueue中的索引
